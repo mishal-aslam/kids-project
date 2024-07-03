4de06f7cf9f5286010b09b91aecd10853c22e720
Mishal Aslam <mishalaslam17@gmail.com>
2024-07-04 02:28:51 +0500

    changes

---
+++ script/Learnalpha.js
@@ -0,0 +1,64 @@
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+const navMenu = document.getElementById("navMenu");
+
+hamburgerBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("hidden");
+  hamburgerBtn.classList.toggle("open");
+});
+
+
+
+function navigateBack() {
+    window.location.href = "login.html";
+}
+
+
+function navigateNext1() {
+    window.location.href = 'agesec.html';
+  }
+
+function navigateNext2() {
+    window.location.href = 'learnit.html';
+  }
+
+  
+
+function navigateNext3() {
+    window.location.href = 'traceit.html';
+  }
+
+
+function navigateNext4() {
+    window.location.href = 'agesec2.html';
+  }
+
+
+function navigateNext5() {
+    window.location.href = 'vowels.html';
+  }
+
+
+function navigateNext6() {
+    window.location.href = 'sayit.html';
+  }
+
+
+function navigateNext7() {
+    window.location.href = 'traceit2.html';
+  }
+
+
+function navigateNext8() {
+    window.location.href = 'agesec3.html';
+  }
+
+
+function navigateNext9() {
+    window.location.href = 'learnit2.html';
+  }
+
+
+function navigateNext10() {
+    window.location.href = 'vocabs.html';
+  }
+
